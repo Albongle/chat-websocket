76fbd82f5427c6caafb83382fa78b706f794384c
Albongle <alejandro.bongioanni@gmail.com>
2022-01-20 15:02:09 -0300

    Visual

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,11 +5,14 @@ const actions = document.querySelector("#websocket-actions");
 const send = document.querySelector("#send");
 const messages = document.querySelector("#websocket-messages");
 
-message.addEventListener("keyup", (event) => {
-  if (user.value === "") {
 
-    alert("Debe ingresar un usuario");
+message.addEventListener("keyup", (event) => {
+  const exp = new RegExp("[a-zA-Z]+[0-9]?");
+  if (!exp.test(user.value)) {
+
+    alert("Debe ingresar un usuario o nombre valido");
     message.value = "";
+    user.value = "";
     user.focus();
   }else if((message.value != "")){
     eventTiping("tiping");
@@ -20,14 +23,31 @@ message.addEventListener("keyup", (event) => {
 
 
 send.addEventListener("click",()=>{
-  eventNewMessage();
-})
+
+  const exp = new RegExp("^ *$");
+  if(message.value.length>0 && !exp.test(message.value)){
+    eventNewMessage();
+  }
+
+  
+});
 
 user.addEventListener("change",()=>{
-    
-    //fetch("/",{method:"POST", headers:{"Content-Type":"application/json; charset=utf-8"},body:JSON.stringify({user:user.value})}).catch(error=>console.error(error));
+  const exp = new RegExp("[a-zA-Z]+[0-9]?");
+  if (exp.test(user.value)) {
+
     document.getElementById("saludo").innerHTML=`Bienvenido ${user.value}`;
-})
+    message.removeAttribute("disabled");
+    send.removeAttribute("disabled");
+  }else{
+    message.setAttribute("disabled",true);
+    send.setAttribute("disabled",true);
+    alert("Debe ingresar un usuario o nombre valido");
+  }
+  
+    //fetch("/",{method:"POST", headers:{"Content-Type":"application/json; charset=utf-8"},body:JSON.stringify({user:user.value})}).catch(error=>console.error(error));
+    
+});
 
 socket.on("chat:tiping", (data) => {
   if(data.status === "tiping"){
@@ -39,7 +59,12 @@ socket.on("chat:tiping", (data) => {
 
 
 socket.on("new:message",(data)=>{
-  messages.innerHTML+=`<p><span>${data.user}:</span> ${data.message}</p>`
+  if(socket.id === data.id){
+    messages.innerHTML+=`<p class="messages-me"><span>${data.user}:</span> ${data.message}</p>`
+  }else{
+    messages.innerHTML+=`<p class="messages-other"><span>${data.user}:</span> ${data.message}</p>`
+  }
+  
 
 });
 
